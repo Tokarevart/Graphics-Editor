4cbf5857074731ff9007db587fd7b13cdd6d3401
Tokarev Artem Alekseevich <tokarev_28@mail.ru>
2018-02-25 18:25:51 +0500

    Update

--- [Pr.] Graphics Editor/Form1.cs
+++ [Pr.] Graphics Editor/Form1.cs
@@ -44,20 +44,27 @@ namespace _Pr.__Graphics_Editor
         {
             FormSize fs = new FormSize();
             fs.ShowDialog();
+
             bmpImage = new Bitmap(fs.width, fs.height, PixelFormat.Format32bppArgb);
             bmpBg = new Bitmap(fs.width, fs.height, PixelFormat.Format32bppArgb);
             bmpBuf = new Bitmap(fs.width, fs.height, PixelFormat.Format32bppArgb);
+
+            //
             // Создает новый объект Graphics из указанного рисунка Image.
             g = Graphics.FromImage(bmpImage);
             g.Clear(Color.Transparent);
             g.Dispose();
+
             g = Graphics.FromImage(bmpBg);
             g.Clear(Color.White);
             g.Dispose();
+
             g = Graphics.FromImage(bmpBuf);
             g.Clear(Color.Transparent);
             g.Dispose();
-            //Задаются свойства для пикчабокс
+
+            //
+            //Задаются свойства для PictureBox
             pictureBox13.Width = fs.width + 2;
             pictureBox13.Height = fs.height + 2;
             pictureBox13.Image = bmpImage;
@@ -67,16 +74,19 @@ namespace _Pr.__Graphics_Editor
         {
             pictureBox13.Image = bmpImage;
             pictureBox13.BackgroundImage = bmpBg;
+
             brushvar = true;
             linevar = false;
             ellivar = false;
             rect4var = false;
             textvar = false;
+
             brush.BackColor = Color.FromArgb(130, 255, 192, 128);
             pbEllipse.BackColor = Color.White;
             pbLine.BackColor = Color.White;
             pbRect.BackColor = Color.White;
             textbtn.BackColor = Color.White;
+
             panel8.Visible = false;
         }
 
@@ -87,23 +97,30 @@ namespace _Pr.__Graphics_Editor
                 if (brushvar == true)
                 {
                     i++;
+
                     g = Graphics.FromImage(bmpImage);
                     g.SmoothingMode = SmoothingMode.HighQuality;
+
                     if (x == 0 && y == 0) { x = e.X; y = e.Y; }
                     g.DrawLine(myPen, x, y, e.X, e.Y);
+
                     Array.Resize<Point>(ref PMas, PMas.Length + 1);
                     PMas[i] = new Point(x, y);
                     x = e.X;
                     y = e.Y;
+
                     pictureBox13.Refresh();
                     g.Dispose();
                 }
                 if (linevar == true)
                 {
                     bmpBuf = new Bitmap(bmpImage);
+
                     g = Graphics.FromImage(bmpBuf);
                     g.SmoothingMode = SmoothingMode.HighQuality;
+
                     g.DrawLine(myPen, x, y, e.X, e.Y);
+
                     pictureBox13.Image = new Bitmap(bmpBuf);
                     pictureBox13.Refresh();
                     g.Dispose();
@@ -111,11 +128,14 @@ namespace _Pr.__Graphics_Editor
                 if (ellivar == true)
                 {
                     bmpBuf = new Bitmap(bmpImage);
+
                     g = Graphics.FromImage(bmpBuf);
                     g.SmoothingMode = SmoothingMode.HighQuality;
+
                     r = new Rectangle(x, y, e.X - x, e.Y - y);
                     if (checkBox1.Checked == true) g.FillEllipse(br, r);
                     g.DrawEllipse(myPen, r);
+
                     pictureBox13.Image = new Bitmap(bmpBuf);
                     pictureBox13.Refresh();
                     g.Dispose();
@@ -123,14 +143,17 @@ namespace _Pr.__Graphics_Editor
                 if (rect4var == true)
                 {
                     bmpBuf = new Bitmap(bmpImage);
+
                     g = Graphics.FromImage(bmpBuf);
                     g.SmoothingMode = SmoothingMode.HighQuality;
+
                     if (e.X >= x && e.Y >= y) r = new Rectangle(x, y, e.X - x, e.Y - y);
                     if (e.X < x && e.Y < y) r = new Rectangle(e.X, e.Y, x - e.X, y - e.Y);
                     if (e.X < x && e.Y >= y) r = new Rectangle(e.X, y, x - e.X, e.Y - y);
                     if (e.X >= x && e.Y < y) r = new Rectangle(x, e.Y, e.X - x, y - e.Y);
                     if (checkBox1.Checked == true) g.FillRectangle(br, r);
                     g.DrawRectangle(myPen, r);
+
                     pictureBox13.Image = new Bitmap(bmpBuf);
                     pictureBox13.Refresh();
                     g.Dispose();
@@ -156,122 +179,162 @@ namespace _Pr.__Graphics_Editor
 
         private void CBlack_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CBlack.BackColor;
-            if (col2var == true) brcolor2.BackColor = CBlack.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CBlack.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CBlack.BackColor;
         }
 
         private void CWhite_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CWhite.BackColor;
-            if (col2var == true) brcolor2.BackColor = CWhite.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CWhite.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CWhite.BackColor;
         }
 
         private void CDimGray_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CDimGray.BackColor;
-            if (col2var == true) brcolor2.BackColor = CDimGray.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CDimGray.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CDimGray.BackColor;
         }
 
         private void CDarkGray_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CDarkGray.BackColor;
-            if (col2var == true) brcolor2.BackColor = CDarkGray.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CDarkGray.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CDarkGray.BackColor;
         }
 
         private void CMaroon_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CMaroon.BackColor;
-            if (col2var == true) brcolor2.BackColor = CMaroon.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CMaroon.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CMaroon.BackColor;
         }
 
         private void CSienna_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CSienna.BackColor;
-            if (col2var == true) brcolor2.BackColor = CSienna.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CSienna.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CSienna.BackColor;
         }
 
         private void CRed_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CRed.BackColor;
-            if (col2var == true) brcolor2.BackColor = CRed.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CRed.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CRed.BackColor;
         }
 
         private void CPink_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CPink.BackColor;
-            if (col2var == true) brcolor2.BackColor = CPink.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CPink.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CPink.BackColor;
         }
 
         private void COrange_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = COrange.BackColor;
-            if (col2var == true) brcolor2.BackColor = COrange.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = COrange.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = COrange.BackColor;
         }
 
         private void CLightOrange_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CLightOrange.BackColor;
-            if (col2var == true) brcolor2.BackColor = CLightOrange.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CLightOrange.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CLightOrange.BackColor;
         }
 
         private void CYellow_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CYellow.BackColor;
-            if (col2var == true) brcolor2.BackColor = CYellow.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CYellow.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CYellow.BackColor;
         }
 
         private void CKhaki_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CKhaki.BackColor;
-            if (col2var == true) brcolor2.BackColor = CKhaki.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CKhaki.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CKhaki.BackColor;
         }
 
         private void CForestGreen_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CForestGreen.BackColor;
-            if (col2var == true) brcolor2.BackColor = CForestGreen.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CForestGreen.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CForestGreen.BackColor;
         }
 
         private void CGreenYellow_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CGreenYellow.BackColor;
-            if (col2var == true) brcolor2.BackColor = CGreenYellow.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CGreenYellow.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CGreenYellow.BackColor;
         }
 
         private void CDodgerBlue_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CDodgerBlue.BackColor;
-            if (col2var == true) brcolor2.BackColor = CDodgerBlue.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CDodgerBlue.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CDodgerBlue.BackColor;
         }
 
         private void CLightSkyBlue_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CLightSkyBlue.BackColor;
-            if (col2var == true) brcolor2.BackColor = CLightSkyBlue.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CLightSkyBlue.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CLightSkyBlue.BackColor;
         }
 
         private void CRoyalBlue_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CRoyalBlue.BackColor;
-            if (col2var == true) brcolor2.BackColor = CRoyalBlue.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CRoyalBlue.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CRoyalBlue.BackColor;
         }
 
         private void CSteelBlue_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CSteelBlue.BackColor;
-            if (col2var == true) brcolor2.BackColor = CSteelBlue.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CSteelBlue.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CSteelBlue.BackColor;
         }
 
         private void CPurple_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CPurple.BackColor;
-            if (col2var == true) brcolor2.BackColor = CPurple.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CPurple.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CPurple.BackColor;
         }
 
         private void CPlum_Click(object sender, EventArgs e)
         {
-            if (col1var == true) brcolor1.BackColor = CPlum.BackColor;
-            if (col2var == true) brcolor2.BackColor = CPlum.BackColor;
+            if (col1var == true) 
+                brcolor1.BackColor = CPlum.BackColor;
+            if (col2var == true) 
+                brcolor2.BackColor = CPlum.BackColor;
         }
 
         private void pictureBox13_MouseUp(object sender, MouseEventArgs e)
